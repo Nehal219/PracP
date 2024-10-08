876969fc6283a2209a3e30a147df9c849c0af7ae
Nehal219 <mahmadnehal502@gmail.com>
2024-10-08 13:49:50 +0530

    Datatypes

---
+++ Datatype1.py
@@ -0,0 +1,17 @@
+a = 10
+b = 77.234
+c = 'r'
+d = 'fg'
+e = True
+f = 4 + 7j
+g = [4,6.5,'h',"hhr",False]
+h = {a : 4,b : "sfgd"}
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
